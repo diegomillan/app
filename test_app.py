a39426d6f899ae35301731aa0b1f52b5997d919b
Diego Millan <diegor.millan@gmail.com>
2023-02-13 14:03:56 -0500

    chore: Using pytest fixtures as tests input.

--- test_app.py
+++ test_app.py
@@ -1,5 +1,10 @@
+import pytest
 from app import find_pairs_with_sum
 
-def test_find_pairs_with_sum():
-    numbers, target = [ 1, 9, 5, 0, 20, -4, 12, 16, 7] , 12
+@pytest.fixture
+def input():
+    return [ 1, 9, 5, 0, 20, -4, 12, 16, 7] , 12
+
+def test_find_pairs_with_sum(input):
+    numbers, target = input
     assert find_pairs_with_sum(numbers, target) == [(0, 12), (-4, 16), (5, 7)]
